3e1a78556c8203c0f45288739a46c6a3cb1002c3
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2022-02-15 15:04:06 +0330

    Add tailwind and functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [tel, setTel] = useState('');
+	const goToWhatsapp = () => {
+		const url = 'https://wa.me/' + tel;
+		window.open(url, '_blank');
+	};
+	return (
+		<div className=" w-full bg-slate-900 h-screen ">
+			<header className="text-center  justify-center items-center flex flex-col App-header">
+				<div className="flex flex-col w-f">
+					<label className=" text-teal-700 mb-2">Please enter phone number:</label>
+					<input
+						type="text"
+						className=" text-blue-900 rounded-lg p-3  shadow-lg"
+						value={tel}
+						onChange={(e) => setTel(e.target.value)}
+					/>
+				</div>
+				<button
+					className="text-3xl text-white font-bold underline bg-teal-700 p-3 m-3 rounded-lg shadow-2xl"
+					onClick={() => goToWhatsapp()}
+				>
+					Go to whatsapp!
+				</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
